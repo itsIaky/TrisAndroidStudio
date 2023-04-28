4712faa8a7ad261306163cca69217836a7e78192
zakka00 <zaka.laoud.2004@gmail.com>
2023-04-28 10:58:44 +0200

    Riassunto win e controllo vittoria
    Tolto il codice del cazzo e messo qualcosa di più umano.

--- app/src/main/java/com/example/tictactoetutorial/MainActivity.java
+++ app/src/main/java/com/example/tictactoetutorial/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.tictactoetutorial;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,8 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     ImageView button1,button2,button3,button4,button5,button6,button7,button8,button9;
     private String startGame="X";
-    int b1=5,b2=5,b3=5,b4=5,b5=5,b6=5,b7=5,b8=5,b9=5,xCount=0,oCount=0,i=0;
-    int c1=0,c2=0,c3=0,c4=0,c5=0,c6=0,c7=0,c8=0,c9=0;
+    int xCount=0,oCount=0,turnCounter=0;
     private TextView scorex,scoreo;
     private Button Reset;
 
@@ -60,23 +58,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                button1.setImageDrawable(null);
-                button2.setImageDrawable(null);
-                button3.setImageDrawable(null);
-                button4.setImageDrawable(null);
-                button5.setImageDrawable(null);
-                button6.setImageDrawable(null);
-                button7.setImageDrawable(null);
-                button8.setImageDrawable(null);
-                button9.setImageDrawable(null);
                 resetValues();
                 xCount=0;
                 oCount=0;
                 scorex.setText("Score X :- "+String.valueOf(xCount));
-                scoreo.setText("Score Y :- "+String.valueOf(oCount));
+                scoreo.setText("Score O :- "+String.valueOf(oCount));
             }
         });
-
         /*
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -403,9 +391,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 buttons[indexPressedButton].setImageResource(R.drawable.circle);
                 buttonsValues[indexPressedButton] = 0;
             }
-            i++;
+            turnCounter++;
             choosePlayer();
-            winningGame();
+            String vincitore = winningGame();
+            if( vincitore != "" ){
+                if( vincitore == "X" ){
+                    xCount++;
+                    scorex.setText("Score X :- "+String.valueOf(xCount));
+                }
+                else{
+                    oCount++;
+                    scoreo.setText("Score O :- "+String.valueOf(oCount));
+                }
+                Toast.makeText(MainActivity.this,"Ha vinto " + vincitore,Toast.LENGTH_SHORT).show();
+                resetValues();
+            }
         }
         else
         {
@@ -414,10 +414,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    private void winningGame()
+    private String winningGame()
     {
-
-
+        /*
         if((b1==1) && (b2==1) && (b3==1))
         {
 
@@ -904,7 +903,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
 
         }
-
+        */
+        String giocatoreVincente = "";
+        for (int i = 0; i < 3; i++)
+        {
+            //controllo vitoria righe
+            if ((buttonsValues[i * 3] + buttonsValues[(i * 3) + 1] + buttonsValues[(i * 3) + 2]) == 3) { giocatoreVincente = "X"; }
+            if ((buttonsValues[i * 3] + buttonsValues[(i * 3) + 1] + buttonsValues[(i * 3) + 2]) == 0) { giocatoreVincente = "O"; }
+            //controllo vittoria colonne
+            if ((buttonsValues[i] + buttonsValues[3 + i] + buttonsValues[6 + i]) == 3) { giocatoreVincente = "X"; }
+            if ((buttonsValues[i] + buttonsValues[3 + i] + buttonsValues[6 + i]) == 0) { giocatoreVincente = "O"; }
+        }
+        //controllo vittoria diagonali
+        if ((buttonsValues[0] + buttonsValues[4] + buttonsValues[8]) == 3) { giocatoreVincente = "X"; }
+        if ((buttonsValues[0] + buttonsValues[4] + buttonsValues[8]) == 0) { giocatoreVincente = "O"; }
+        if ((buttonsValues[2] + buttonsValues[4] + buttonsValues[6]) == 3) { giocatoreVincente = "X"; }
+        if ((buttonsValues[2] + buttonsValues[4] + buttonsValues[6]) == 0) { giocatoreVincente = "O"; }
+        return giocatoreVincente;
     }
 
 
@@ -923,30 +938,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private void resetValues() {
-
-        b1=5;
-        b2=5;;
-        b3=5;
-        b4=5;
-        b5=5;
-        b6=5;
-        b7=5;
-        b8=5;
-        b9=5;
-        i=0;
-        c1=0;
-        c2=0;
-
-        c3=0;
-        c4=0;
-        c5=0;
-        c6=0;
-        c7=0;
-        c8=0;
-        c9=0;
-
-
-
-
+        turnCounter=0;
+        for( int i = 0; i < buttonsValues.length; i++ ){
+            buttonsValues[i] = 10;
+        }
+        button1=findViewById(R.id.buttonImage1);
+        button2=findViewById(R.id.buttonImage2);
+        button3=findViewById(R.id.buttonImage3);
+        button4=findViewById(R.id.buttonImage4);
+        button5=findViewById(R.id.buttonImage5);
+        button6=findViewById(R.id.buttonImage6);
+        button7=findViewById(R.id.buttonImage7);
+        button8=findViewById(R.id.buttonImage8);
+        button9=findViewById(R.id.buttonImage9);
+        button1.setImageDrawable(null);
+        button2.setImageDrawable(null);
+        button3.setImageDrawable(null);
+        button4.setImageDrawable(null);
+        button5.setImageDrawable(null);
+        button6.setImageDrawable(null);
+        button7.setImageDrawable(null);
+        button8.setImageDrawable(null);
+        button9.setImageDrawable(null);
     }
 }
